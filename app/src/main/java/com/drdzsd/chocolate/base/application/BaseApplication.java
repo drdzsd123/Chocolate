3d41a4423a6aebb3ea26a2471502ae2ea4f322fe
drdzsd123 <303494371@qq.com>
2019-09-30 15:04:04 +0800

    first commit

---
+++ app/src/main/java/com/drdzsd/chocolate/base/application/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.drdzsd.chocolate.base.application;
+
+import com.qihoo360.replugin.RePluginApplication;
+
+/**
+ * Created by zsd on 2019/8/29
+ */
+public class BaseApplication extends RePluginApplication {
+
+
+
+}
